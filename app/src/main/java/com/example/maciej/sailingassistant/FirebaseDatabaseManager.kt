9702ca4fc61f1ff40ca88b71187732df47bd5407
dagmaragluch <Dagmara320@gmail.com>
2019-05-04 02:19:50 +0200

    przekazanie i obsługa danych z FirebaseManager do MainActivity
    Signed-off-by: dagmaragluch <Dagmara320@gmail.com>

--- app/src/main/java/com/example/maciej/sailingassistant/FirebaseDatabaseManager.kt
+++ app/src/main/java/com/example/maciej/sailingassistant/FirebaseDatabaseManager.kt
@@ -2,33 +2,39 @@ package com.example.maciej.sailingassistant
 
 import android.util.Log
 import com.google.firebase.database.*
+//import sun.awt.windows.ThemeReader.getPosition
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.ValueEventListener
+
 
 /**
  * Zarządza onlinową bazą danych firebase
  */
 class FirebaseDatabaseManager {
     private val db: DatabaseReference = FirebaseDatabase.getInstance().reference
+    var points = ArrayList<Point?>()
 
-    fun fetchPoints(startDatetime: Datetime, endDatetime: Datetime){
-
+    fun fetchPoints(startDatetime: Datetime, endDatetime: Datetime, firebaseCallback: FirebaseCallback) {
         val query = db.child("boats").child("twR0OqZbv3b875Y8NSnjR39leZi2").orderByKey().startAt(startDatetime.toFormattedString()).endAt(endDatetime.toFormattedString())
         query.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onCancelled(p0: DatabaseError) {
-                Log.d("myDB",p0.message)
+                Log.d("myDB", p0.message)
             }
 
             override fun onDataChange(p0: DataSnapshot) {
-                val points = ArrayList<Point?>()
                 var currentPoint: Point?
-                for(a in p0.children) {
-                    Log.d("myDB",a.key)
-                    currentPoint=a.getValue(Point::class.java)
+                for (a in p0.children) {
+                    Log.d("myDB", a.key)
+                    currentPoint = a.getValue(Point::class.java)
                     currentPoint?.datetime = Datetime.fromString(a.key!!)
                     points.add(currentPoint)
-                    Log.d("myDB",currentPoint.toString())
+                    Log.d("myDB", currentPoint.toString())
                 }
-                //TODO przekazanie utowrzonej listy punktów do main activity
+                firebaseCallback.onCallback(points) //żeby można było użyć danych poza tą metodą
             }
         })
+
     }
+
 }
--- app/src/main/java/com/example/maciej/sailingassistant/MainActivity.kt
+++ app/src/main/java/com/example/maciej/sailingassistant/MainActivity.kt
@@ -4,30 +4,58 @@ import android.content.Intent
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.PolylineOptions
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapClickListener {
 
     lateinit var map: GoogleMap
     private lateinit var mapView: MapView
+    private val firebaseDatabaseManager = FirebaseDatabaseManager()
+    var startDatetime: Datetime = Datetime.fromString("2019-05-02T20:27:19:154Z")
+    var endDatetime: Datetime = Datetime.fromString("2019-05-02T20:27:27:215Z")
+
 
     override fun onMapClick(p0: LatLng?) {
         println(p0)
     }
 
     override fun onMapReady(map: GoogleMap?) {
-        if( map != null ) this.map = map
-        map?.setMinZoomPreference(12.0f)
+        if (map != null) this.map = map
+        map?.setMinZoomPreference(1.0f)
         val ny = LatLng(40.7143528, -74.0059731)
         map?.moveCamera(CameraUpdateFactory.newLatLng(ny))
         map?.addPolyline(PolylineOptions().add(ny, LatLng(40.7243528, -74.0159731)).width(5.0f).color(Color.RED))
         map?.setOnMapClickListener(this)
+
+        firebaseDatabaseManager.fetchPoints(startDatetime, endDatetime, object : FirebaseCallback {
+            override fun onCallback(points: ArrayList<Point?>) {
+                Log.d("TAG", points.toString())
+                println("test 1 = ${points[3]}")
+                println("test 2 = ${points[3]!!.latitude}")
+
+                var startLatitude = points[0]!!.latitude
+                var startLongitude = points[0]!!.longitude
+//                var endLatitude = points[1]!!.latitude - 15
+//                var endLongitude = points[1]!!.longitude - 11
+
+                val ny2 = LatLng(startLatitude, startLongitude)
+                map?.moveCamera(CameraUpdateFactory.newLatLng(ny2))
+//                map?.addPolyline(PolylineOptions().add(ny2, LatLng(endLatitude, endLongitude)).width(5.0f).color(Color.BLUE))
+
+                for (i in 0..points.size-2 ){   //robocze rysowanie trasy
+                    map?.addPolyline(PolylineOptions().add(LatLng(points[i]!!.latitude, points[i]!!.longitude), LatLng(points[i+1]!!.latitude, points[i+1]!!.longitude)).width(5.0f).color(Color.BLUE))
+                }
+            }
+        })
+
+
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -69,9 +97,10 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapCli
         mapView.onLowMemory()
     }
 
-    fun launchDetailActivity(points : ArrayList<Point>) {
-        val detailIntent = Intent(this,DetailActivity::class.java)
-        detailIntent.putParcelableArrayListExtra("points",points)
+
+    fun launchDetailActivity(points: ArrayList<Point>) {
+        val detailIntent = Intent(this, DetailActivity::class.java)
+        detailIntent.putParcelableArrayListExtra("points", points)
         startActivity(detailIntent)
     }
 }
